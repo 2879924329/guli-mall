34d4f0cbb0b9a5482079d31ccec9e2302d7a132e
shmily_wch <2879924329@qq.com>
2022-03-09 09:32:40 +0800

    集成es全文检索

---
+++ gulimall-search/src/main/java/com/wch/gulimall/search/config/ElasticSearchConfig.java
@@ -0,0 +1,39 @@
+package com.wch.gulimall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wch
+ * @version 1.0
+ * @date 2022/3/8 22:57
+ * <p>
+ * es配置
+ * 给容器中注入RestHighLevelClient
+ * api:https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high.html
+ */
+@Configuration
+public class ElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+       /* builder.addHeader("Authorization", "Bearer " + TOKEN);
+        builder.setHttpAsyncResponseConsumerFactory(
+                new HttpAsyncResponseConsumerFactory
+                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));*/
+        COMMON_OPTIONS = builder.build();
+    }
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.84.88", 9201, "http")));
+        return restHighLevelClient;
+    }
+}
